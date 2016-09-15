3e0ede506db50eff9b3b366fcbbb1cc4a282f7ab
Axel Bellec <axel.bellec@outlook.fr>
2016-09-15 16:50:43 +0200

    add a condition to only include data older than 1 hour ago

--- public/views.py
+++ public/views.py
@@ -39,7 +39,8 @@ def index(request):
     WHERE
         persons.team_id = teams.id AND
         submissions.by_id = persons.id AND
-        schools.id = teams.school_id
+        schools.id = teams.school_id AND
+        submissions.at < datetime('now', '-1 hour')
     GROUP BY
         teams.id;
     ''')
@@ -59,15 +60,18 @@ def register(request):
         user = User.objects.filter(email=form['email']).first()
         # Check if the user already exists
         if user:
-            messages.error(request, 'Cette adresse email est déjà associée à un compte.')
+            messages.error(
+                request, 'Cette adresse email est déjà associée à un compte.')
             return render(request, 'public/auth/registration.html')
         person = Person(
             first_name=form['first_name'],
             last_name=form['last_name'],
-            date_of_birth=dt.datetime.strptime(form['date_of_birth'], '%d/%m/%Y')
+            date_of_birth=dt.datetime.strptime(
+                form['date_of_birth'], '%d/%m/%Y')
         )
         person.save()
-        user = person.create_user(email=form['email'], password=form['password1'])
+        user = person.create_user(
+            email=form['email'], password=form['password1'])
         # Send a confirmation email
         token = TS.dumps(form['email'], salt='email-confirm-key')
         context = {
@@ -77,7 +81,8 @@ def register(request):
         subject = 'Confirmation de compte'
         sender = 'noreply.aidor@gmail.com'
         recipient = form['email']
-        message = render_to_string('public/email/auth/registration_confirmation.html', context)
+        message = render_to_string(
+            'public/email/auth/registration_confirmation.html', context)
         msg = EmailMessage(subject, message, sender, [recipient])
         msg.content_subtype = 'html'
         msg.send()
@@ -99,7 +104,8 @@ def confirm_registration(request, token):
     user.is_active = True
     user.save()
     # Notify the user and send him to the login page
-    messages.success(request, 'Votre compte a bien été activé! Vous pouvez à présent vous connecter.')
+    messages.success(
+        request, 'Votre compte a bien été activé! Vous pouvez à présent vous connecter.')
     return redirect('public:login')
 
 
@@ -114,10 +120,12 @@ def login_user(request):
                 login(request, user)
                 return redirect('public:account')
             else:
-                messages.error(request, "L'adresse email de ce compte n'a pas été confirmée.")
+                messages.error(
+                    request, "L'adresse email de ce compte n'a pas été confirmée.")
                 return render(request, 'public/auth/login.html')
         else:
-            messages.error(request, "Les identifiants que vous nous avez fournis sont invalides.")
+            messages.error(
+                request, "Les identifiants que vous nous avez fournis sont invalides.")
             return render(request, 'public/auth/login.html')
     return render(request, 'public/auth/login.html')
 
@@ -133,7 +141,8 @@ def password_forgotten(request):
         user = User.objects.filter(email=form['email']).first()
         # Check if the user exists
         if not user:
-            messages.error(request, "Aucun compte n'est associée à cette adresse email.")
+            messages.error(
+                request, "Aucun compte n'est associée à cette adresse email.")
             return render(request, 'public/auth/password_forgotten.html')
         # Send a reset email
         token = TS.dumps(form['email'], salt='email-reset-key')
@@ -144,7 +153,8 @@ def password_forgotten(request):
         subject = 'Réinitialisation de mot de passe'
         sender = 'noreply.aidor@gmail.com'
         recipient = form['email']
-        message = render_to_string('public/email/auth/password_forgotten_confirmation.html', context)
+        message = render_to_string(
+            'public/email/auth/password_forgotten_confirmation.html', context)
         msg = EmailMessage(subject, message, sender, [recipient])
         msg.content_subtype = 'html'
         msg.send()
@@ -168,15 +178,18 @@ def password_reset(request, token):
         if user:
             # Check the account has been confirmed
             if user.is_active is False:
-                messages.error(request, "L'adresse email de ce compte n'a pas été confirmée.")
+                messages.error(
+                    request, "L'adresse email de ce compte n'a pas été confirmée.")
                 return render(request, 'public/auth/password_forgotten.html')
             user.set_password(form['password1'])
             user.save()
             # Notify the user and send him to the login page
-            messages.success(request, 'Votre mot de passe a bien été changé. Vous pouvez à présent vous connecter.')
+            messages.success(
+                request, 'Votre mot de passe a bien été changé. Vous pouvez à présent vous connecter.')
             return redirect('public:login')
         else:
-            messages.error(request, "Aucun compte n'est associée à cette adresse email.")
+            messages.error(
+                request, "Aucun compte n'est associée à cette adresse email.")
             return render(request, 'public/auth/password_forgotten.html')
     context = {
         'token': token,
@@ -244,7 +257,8 @@ def join_team(request):
     msg.content_subtype = 'html'
     msg.send()
     # Redirect and notify the user that the email has been sent
-    messages.success(request, "Votre demande a été envoyée par email au capitaine de l'équipe.")
+    messages.success(
+        request, "Votre demande a été envoyée par email au capitaine de l'équipe.")
     return redirect('public:index')
 
 
@@ -260,7 +274,8 @@ def accept_member(request, token, person_id):
     person.team = request.user.person.team
     person.save()
 
-    messages.success(request, '{} a rejoint votre équipe.'.format(person.full_name))
+    messages.success(
+        request, '{} a rejoint votre équipe.'.format(person.full_name))
     return redirect('public:account')
 
 
@@ -299,7 +314,8 @@ def remove_team_member(request, person_id):
     person.team = None
     person.save()
 
-    messages.success(request, '{} a été retiré de votre équipe.'.format(person.full_name))
+    messages.success(
+        request, '{} a été retiré de votre équipe.'.format(person.full_name))
     return redirect('public:account')
 
 
@@ -315,5 +331,6 @@ def name_captain(request, person_id):
     person.is_captain = True
     person.save()
 
-    messages.success(request, '{} est maintenant le capitaine de votre équipe.'.format(person.full_name))
+    messages.success(
+        request, '{} est maintenant le capitaine de votre équipe.'.format(person.full_name))
     return redirect('public:account')
